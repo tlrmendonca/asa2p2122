58832e852bca0d4ee19eda186a62b4c3cc714f91
Tomás Mendonça <tomas.mendonca.01@gmail.com>
2022-01-17 15:10:09 +0000

    Added main file

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int problem;
+    vector<int> sequence1, sequence2;
+
+    // Data input
+    // char *seq = NULL;
+    // if (scanf("%i\n%m[^\n]%*c", &problem, &seq) != 2)
+    //     cout << "Debug: fscanf" << endl;
+    // sequence1 = to_vector(seq);
+
+    // if (problem == 2)
+    // {
+    //     int temp;
+    //     while (scanf("%d", &temp) == 1)
+    //         sequence2.push_back(temp);
+    // }
+
+    // Solution
+    return 0;
+}
